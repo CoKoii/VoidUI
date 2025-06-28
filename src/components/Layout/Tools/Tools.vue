8f6da2a4cc277c5e1cd9e7f056d6e777090416ee
CaoKai <2655257336@qq.com>
2025-06-29 06:52:02 +0800

    调整导航栏样式，优化 logo 间距和字体大小；新增主题切换功能；更新基础样式以支持主题；新增主题样式文件

--- src/components/Layout/Tools/Tools.vue
+++ src/components/Layout/Tools/Tools.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
 import { Tooltip } from 'ant-design-vue'
-import { GithubFilled } from '@ant-design/icons-vue'
+import { GithubFilled, SettingFilled } from '@ant-design/icons-vue'
 defineExpose({
   name: 'Tools',
 })
+const changeTheme = () => {
+  const htmlElement = document.documentElement
+  if (htmlElement.classList.contains('dark')) {
+    htmlElement.classList.remove('dark')
+    localStorage.setItem('theme', 'light')
+  } else {
+    htmlElement.classList.add('dark')
+    localStorage.setItem('theme', 'dark')
+  }
+}
 </script>
 
 <template>
@@ -12,7 +22,7 @@ defineExpose({
       <template #title>
         <span>Github</span>
       </template>
-      <GithubFilled :style="{ fontSize: '14px', color: '#000' }" />
+      <SettingFilled :style="{ fontSize: '14px', color: '#000' }" @click="changeTheme" />
     </Tooltip>
     <Tooltip placement="bottom">
       <template #title>
