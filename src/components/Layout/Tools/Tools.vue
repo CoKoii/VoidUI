357a9169b5145b25ea2c22e4c6aee25098e3a8e1
CaoKai <2655257336@qq.com>
2025-06-29 10:06:40 +0800

    优化菜单项悬停效果，新增过渡动画；调整导航栏 logo 尺寸；更新工具组件图标为灯泡，修改主题切换提示文本；增强基础样式的背景和文字颜色过渡效果

--- src/components/Layout/Tools/Tools.vue
+++ src/components/Layout/Tools/Tools.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { Tooltip } from 'ant-design-vue'
-import { GithubFilled, SettingFilled } from '@ant-design/icons-vue'
+import { GithubFilled, BulbFilled } from '@ant-design/icons-vue'
 defineExpose({
   name: 'Tools',
 })
+
 const changeTheme = () => {
   const htmlElement = document.documentElement
   if (htmlElement.classList.contains('dark')) {
@@ -20,15 +21,9 @@ const changeTheme = () => {
   <div class="Tools">
     <Tooltip placement="bottom">
       <template #title>
-        <span>Github</span>
+        <span>日/夜</span>
       </template>
-      <SettingFilled :style="{ fontSize: '14px', color: '#000' }" @click="changeTheme" />
-    </Tooltip>
-    <Tooltip placement="bottom">
-      <template #title>
-        <span>Github</span>
-      </template>
-      <GithubFilled :style="{ fontSize: '14px', color: '#000' }" />
+      <BulbFilled :style="{ fontSize: '14px', color: '#000' }" @click="changeTheme" />
     </Tooltip>
     <Tooltip placement="bottom">
       <template #title>
