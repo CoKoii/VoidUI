2310e943d3086aaaf64f6decd8a4b77991494661
CaoKai <2655257336@qq.com>
2025-06-29 18:06:04 +0800

    重构首页组件，添加标题、描述和按钮样式

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
+import { Button } from 'ant-design-vue'
+import { SendOutlined } from '@ant-design/icons-vue'
+import { defineExpose, h } from 'vue'
 defineExpose({
   name: 'Home',
 })
 </script>
 
 <template>
-  <div class="Home">首页</div>
+  <div class="Home">
+    <h1 class="title">Void Design</h1>
+    <p class="description">一切从简，安然如山水。</p>
+    <div class="buttons">
+      <Button type="primary" @click="$router.push('/components')" :icon="h(SendOutlined)"
+        >开始使用</Button
+      >
+      <Button @click="$router.push('/about')">了解更多</Button>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+@use 'Home.scss';
+</style>
