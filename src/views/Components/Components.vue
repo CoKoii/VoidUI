a4b2aaead2c9ec49ca9fb073190a206422086cba
CaoKai <2655257336@qq.com>
2025-07-01 18:19:10 +0800

    实现导航栏的展开与收起功能，优化样式和交互体验

--- src/views/Components/Components.vue
+++ src/views/Components/Components.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons-vue'
+const isNavOpen = ref(false)
+const toggleNav = () => {
+  isNavOpen.value = !isNavOpen.value
+}
 defineExpose({
   name: 'Components',
 })
@@ -6,8 +12,12 @@ defineExpose({
 
 <template>
   <div class="Components">
-    <div class="nav">导航栏</div>
+    <div class="nav" :class="{ open: isNavOpen }">导航栏</div>
     <div class="main">主体区域</div>
+    <div class="nav-toggle" @click="toggleNav">
+      <MenuUnfoldOutlined v-if="!isNavOpen" />
+      <MenuFoldOutlined v-if="isNavOpen" />
+    </div>
   </div>
 </template>
 
