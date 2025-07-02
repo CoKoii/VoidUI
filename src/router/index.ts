570a912cbb2e92fe0428baf4e480d55de86355b6
CaoKai <2655257336@qq.com>
2025-07-02 14:01:47 +0800

    添加重定向到组件页面的按钮子路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,7 @@ const router = createRouter({
       path: '/components',
       name: 'Components',
       component: () => import('@/views/Components/Components.vue'),
+      redirect: '/components/button',
       children: ComponentsChildren(),
     },
     {
