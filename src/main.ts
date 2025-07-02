2986fa5ccc36c7a61e00589e03d2688b873490b8
CaoKai <2655257336@qq.com>
2025-07-02 17:05:59 +0800

    添加 CodePreview 组件并注册，更新主题样式以支持图标颜色和背景色

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ import '@/styles/theme.scss'
 import '@/styles/base.scss'
 import App from './App.vue'
 import router from './router'
-
+import CodePreview from './components/CodePreview/CodePreview.vue'
 const app = createApp(App)
-
+app.component('CodePreview', CodePreview)
 app.use(createPinia())
 app.use(router)
 app.use(Antd)
