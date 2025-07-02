9b3b88d04f43a1f59480b9fdb03e8177360e95b3
CaoKai <2655257336@qq.com>
2025-07-02 17:04:42 +0800

    添加 CodePreview 组件及样式，更新 Button 组件以支持代码预览功能

--- src/VoidComponents/Button/Button.vue
+++ src/VoidComponents/Button/Button.vue
@@ -16,6 +16,22 @@ defineExpose({
         按钮有三种类型：<code>primary</code> 、<code>secondary</code> 、<code>outline</code>
         。主按钮在同一个操作区域建议最多出现一次。
       </p>
+      <CodePreview
+        :code="`<script setup lang=&quot;ts&quot;>
+import ArticleSide from '@/components/ArticleSide/ArticleSide.vue'
+
+defineExpose({
+  name: 'Button',
+})
+</script>`"
+        language="javascript"
+      >
+        <template #preview>
+          <a-button type="primary">Primary</a-button>
+          <a-button type="secondary">Secondary</a-button>
+          <a-button type="outline">Outline</a-button>
+        </template>
+      </CodePreview>
     </ArticleSide>
   </div>
 </template>
---
+++ src/components/CodePreview/CodePreview.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { CopyOutlined, CodeOutlined } from '@ant-design/icons-vue'
+defineExpose({
+  name: 'CodePreview',
+})
+</script>
+
+<template>
+  <div class="CodePreview">
+    <div class="preview">
+      <slot name="preview"></slot>
+    </div>
+    <div class="tools">
+      <CopyOutlined />
+      <CodeOutlined />
+    </div>
+    <pre class="code">
+      <slot name="code"></slot>
+    </pre>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use './CodePreview.scss';
+</style>
