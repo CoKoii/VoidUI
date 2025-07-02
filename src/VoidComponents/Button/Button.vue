287691e5c67939a3ea1a0e8c7deba7e50bc15d56
CaoKai <2655257336@qq.com>
2025-07-02 14:57:39 +0800

    重构 Button 组件，添加 ArticleSide 组件并更新样式；新增 ArticleSide 和 markdown 样式文件

--- src/VoidComponents/Button/Button.vue
+++ src/VoidComponents/Button/Button.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
+import ArticleSide from '@/components/ArticleSide/ArticleSide.vue'
 defineExpose({
   name: 'Button',
 })
 </script>
 
 <template>
-  <div class="Button">按钮</div>
+  <div class="Button">
+    <ArticleSide>
+      <h1>Button 组件</h1>
+      <p>点击开始一个即时操作</p>
+      <h2 id="button-types">按钮类型</h2>
+    </ArticleSide>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/ArticleSide/ArticleSide.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { Anchor } from 'ant-design-vue'
+
+defineExpose({
+  name: 'ArticleSide',
+})
+</script>
+
+<template>
+  <div class="ArticleSide">
+    <div class="main">
+      <slot></slot>
+    </div>
+    <div class="nav">
+      <Anchor
+        :affix="false"
+        :items="[
+          {
+            key: '1',
+            href: '#button-types',
+            title: '按钮类型',
+          },
+          {
+            key: '2',
+            href: '#components-anchor-demo-static',
+            title: 'Static demo',
+          },
+          {
+            key: '3',
+            href: '#api',
+            title: 'API',
+            children: [
+              {
+                key: '4',
+                href: '#anchor-props',
+                title: 'Anchor Props',
+              },
+              {
+                key: '5',
+                href: '#link-props',
+                title: 'Link Props',
+              },
+            ],
+          },
+        ]"
+      ></Anchor>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@use './ArticleSide.scss';
+@use './markdown.scss';
+</style>
