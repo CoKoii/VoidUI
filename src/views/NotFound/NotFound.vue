94deb9a7409d89ca725ecb550a622fdb5b77bf1d
CK <2655257336@qq.com>
2025-08-28 15:52:43 +0800

    优化 NotFound 组件的样式，调整标题文本并添加响应式设计

--- src/views/NotFound/NotFound.vue
+++ src/views/NotFound/NotFound.vue
@@ -6,8 +6,34 @@ defineExpose({
 
 <template>
   <div class="NotFound">
-    <h1>404 - Not Found</h1>
+    <h1>404 Not Found</h1>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.NotFound {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 80px);
+  position: relative;
+  h1 {
+    position: absolute;
+    top: 35%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 80px;
+    font-weight: bold;
+  }
+}
+@media screen and (max-width: 768px) {
+  .NotFound {
+    height: calc(100vh - 60px);
+
+    h1 {
+      font-size: 30px;
+      text-align: center;
+    }
+  }
+}
+</style>
